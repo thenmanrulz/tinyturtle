5002dd7d6379306d9901d6214ea6911fc0310ecf
noah lopez <thenman534@gmail.com>
2017-02-16 20:36:44 +0000

    my comment

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-  TinyTurtle.apply(window); 
+ TinyTurtle.apply(window); 
+ 
+ /*Global Tinyturtle*/
+ 
+ forward(50);
+ right(90);
+ forward(50);
+ right(90);
+ forward(50);
+ right(90);
+ forward(50);
+ right(90);
+ forward(50);
+ right(45);
+ forward(37);
+ right(90);
+ forward(35);
+ right(220);
+ forward(10);
+ right(90);
+ forward(7);
+ right(90);
+ forward(20);
+ right(90);
+ forward(7);
+ 
+ stamp();
